0936ada64801adf3c85fd3e47349ad55e48bfd49
Kanet.kam <>
2026-05-09 16:09:38 +0700

    feat: add start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_flashcard_quiz_app/start_screen.dart';
+
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text('Flutter Quiz App'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 233, 75, 201),
+                Color.fromARGB(255, 129, 243, 211),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
-  ));
+  );
 }
