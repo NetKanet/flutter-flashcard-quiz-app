a2149401926c77a14c1f4bd6925500ca20307a4a
Kanet.kam <>
2026-05-11 17:12:06 +0700

    feat: add quiz question model and questions data

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,6 +22,10 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidget = activeScreen == 'start-screen'
+        ? StartScreen(switchScreen)
+        : const QuestionsScreen();
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -35,9 +39,7 @@ class _QuizState extends State<Quiz> {
               end: AlignmentGeometry.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
