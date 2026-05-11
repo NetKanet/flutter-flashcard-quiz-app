52a9179945ed75bf86b3a87c2dbb56f512ff48cf
Kanet.kam <>
2026-05-11 20:56:22 +0700

    feat: render questions and answers dynamically

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_flashcard_quiz_app/answer_button.dart';
+import 'package:flutter_flashcard_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,17 +13,20 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Question ...'),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
           const SizedBox(height: 30),
-          AnswerButton(answerText: 'Answer1', onTap: () {}),
-          AnswerButton(answerText: 'Answer2', onTap: () {}),
-          AnswerButton(answerText: 'Answer3', onTap: () {}),
-          AnswerButton(answerText: 'Answer4', onTap: () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );
