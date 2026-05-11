e66600202a93c0b3096009db440fab4a6b3d7108
Kanet.kam <>
2026-05-12 02:45:06 +0700

    feat: display quiz results with score and answer summary

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_flashcard_quiz_app/data/questions.dart';
+import 'package:flutter_flashcard_quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswer});
 
   final List<String> chosenAnswer;
 
-  List<Map<String, Object>> getSummarry() {
+  List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < chosenAnswer.length; i++) {
@@ -23,6 +24,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -30,9 +37,9 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text('You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!'),
             const SizedBox(height: 30),
-            const Text('List of answer and questions...'),
+            Questionssummary(summaryData),
             const SizedBox(height: 30),
             TextButton(onPressed: () {}, child: const Text('Restaer Quiz!')),
           ],
