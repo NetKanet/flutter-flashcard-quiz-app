d7adc57a105ebec2abc62648e3094ac5ee3a0e96
Kanet.kam <>
2026-05-11 17:53:56 +0700

    feat: add reusable AnswerButton widget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_flashcard_quiz_app/answer_button.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -10,6 +12,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Hello welcome');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question ...'),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer1', onTap: () {}),
+          AnswerButton(answerText: 'Answer2', onTap: () {}),
+          AnswerButton(answerText: 'Answer3', onTap: () {}),
+          AnswerButton(answerText: 'Answer4', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
