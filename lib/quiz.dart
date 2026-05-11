147b89c112fe36f9e62c1f8b5e4b90f7df1668c4
Kanet.kam <>
2026-05-12 01:46:53 +0700

    feat: add results screen with answer summary data

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flashcard_quiz_app/data/questions.dart';
 import 'package:flutter_flashcard_quiz_app/questions_screen.dart';
 import 'package:flutter_flashcard_quiz_app/start_screen.dart';
+import 'package:flutter_flashcard_quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -27,16 +28,22 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidget = activeScreen == 'start-screen'
-        ? StartScreen(switchScreen)
-        : QuestionsScreen(onSelectAnswer: chooseAnswer);
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(chosenAnswer: selectedAnswers);
+    }
 
     return MaterialApp(
       home: Scaffold(
