6fc148d069c6ec569d1b19913eee7d2c3b5b810d
Kanet.kam <>
2026-05-12 00:48:49 +0700

    feat: reset quiz and return to start after last question

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flashcard_quiz_app/questions_screen.dart';
 
+import 'package:flutter_flashcard_quiz_app/data/questions.dart';
+import 'package:flutter_flashcard_quiz_app/questions_screen.dart';
 import 'package:flutter_flashcard_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -11,7 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -22,6 +23,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
