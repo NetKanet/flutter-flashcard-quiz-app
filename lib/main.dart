9436527ee3996c861ee7fff54a0d0a36a4465ad4
Kanet.kam <>
2026-05-11 16:24:16 +0700

    feat: add quiz state management and screen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_flashcard_quiz_app/start_screen.dart';
+import 'package:flutter_flashcard_quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 233, 75, 201),
-                Color.fromARGB(255, 129, 243, 211),
-              ],
-              begin: AlignmentGeometry.topLeft,
-              end: AlignmentGeometry.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flashcard_quiz_app/questions_screen.dart';
+
+import 'package:flutter_flashcard_quiz_app/start_screen.dart';
+import 'package:flutter_flashcard_quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 233, 75, 201),
+                Color.fromARGB(255, 129, 243, 211),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : QuestionsScreen(),
+        ),
+      ),
+    );
+  }
+}
